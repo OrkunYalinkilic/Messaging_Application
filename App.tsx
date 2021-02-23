45a30ed3a3f743a3299ba2c32eca76a357850ff2
Orkun Yalınkılıç <orkunyalinkilic69@gmail.com>
2021-02-23 23:53:18 +0300

    2_Firebase_Baglanti_Auth

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import AppContainer from "./AppContainer";
 import firebase from 'firebase';
+
+const { width, height } = Dimensions.get('window');
 
 interface IProps { }
 interface IState { }
@@ -13,6 +15,11 @@ export default class App extends React.Component<IProps, IState> {
     super(p, s);
   }
 
+  state = {
+    email: '',
+    password: '',
+    login: false
+  }
 
   componentDidMount = () => {
     var firebaseConfig = {
@@ -28,19 +35,58 @@ export default class App extends React.Component<IProps, IState> {
 
     firebase.auth().onAuthStateChanged(auth => {
       if (auth) {
-        //giriş yapıldı
+        console.log("Giriş Yapıldı");
+
       }
       else {
-        //giriş yapılmadı
+        console.log("Giriş Yapılmadı");
+
       }
     })
-
   }
 
-  
+  kayitol = () => {
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
+  }
+
+  giris = () => {
+    firebase.auth().signInWithEmailAndPassword(this.state.email,this.state.password);
+  }
+
   render() {
     debugger;
-    return (<AppContainer />);
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
+        <TextInput
+          placeholder="E-Mail Adresi"
+          style={{ marginTop: 10, width: '80%', padding: 15, fontSize: 14, backgroundColor: 'lightgray' }}
+          underlineColorAndroid='transparent'
+          onChangeText={email => this.setState({ email: email })}
+          value={this.state.email}
+          placeholderTextColor='gray'
+        />
+        <TextInput
+          placeholder="Şifre"
+          style={{ marginTop: 10, width: '80%', padding: 15, fontSize: 14, backgroundColor: 'lightgray' }}
+          underlineColorAndroid='transparent'
+          onChangeText={password => this.setState({ password: password })}
+          value={this.state.password}
+          secureTextEntry
+          placeholderTextColor='gray'
+        />
+        <TouchableOpacity onPress={() => this.kayitol()} style={{ width: '80%' }}>
+          <View style={{ alignItems: 'center', backgroundColor: '#FF655B', width: '100%', padding: 15, borderRadius: 4, marginTop: 10 }}>
+            <Text style={{ color: '#fff', fontSize: 14 }}>Kayıt Ol</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>this.giris()} style={{ width: '80%' }}>
+          <View style={{ alignItems: 'center', backgroundColor: '#FF655B', width: '100%', padding: 15, borderRadius: 4, marginTop: 10 }}>
+            <Text style={{ color: '#fff', fontSize: 14 }}>Giriş Yap</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
   }
 }
 
